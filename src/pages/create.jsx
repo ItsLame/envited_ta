ce477632d0e340bc6510ce32329ecc02bc06b429
ItsLame <37344470+ItsLame@users.noreply.github.com>
2022-09-30 23:09:17 +1000

    create page

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles/create.css';
+import PurpleButton from '../components/button';
 
 function Create() {
     useEffect(() => {
@@ -6,8 +10,30 @@ function Create() {
     });
 
     return (
-        <div>
-            <h1>heyCreate</h1>
+        <div className='create-container'>
+                <h1>Creating Your Event</h1>
+                {/* <p>Hosted by <span id='bold'>Elysia</span></p> */}
+                <div className='form'>
+                    <div id='form-row'>
+                        <label for='name'>Event Name</label>
+                        <input id='name' type='field'></input>
+                    </div>
+                    <div id='form-row'>
+                        <label for='name'>Host Name</label>
+                        <input id='name' type='field'></input>
+                    </div>
+                    <div id='form-row'>
+                        <label for='name'>From</label>
+                        <input id='name' type='date'></input>
+                    </div>
+                    <div id='form-row'>
+                        <label for='name'>To</label>
+                        <input id='name' type='date'></input>
+                    </div>
+                    <Link to='/event'>
+                        <PurpleButton customText='🚀 Create' />
+                    </Link>
+                </div>
         </div>
     );
 }
